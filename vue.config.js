96774b8a3763ab2a99f6bb6694661bbcbb9f8697
Mattias <mattias-kallstrom@hotmail.com>
2022-05-09 18:46:39 +0200

    Update name

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/Vue-todo/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/Vuetodo/" : "/",
   devServer: {
     proxy: {
       '^/api': {
